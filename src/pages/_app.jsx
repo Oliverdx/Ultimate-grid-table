fdbe7585e30b0fff6e955de49d8c227f12ef0299
Vanderlei Oliveira <vanderlei327@gmail.com>
2021-05-26 12:50:11 -0300

    remove Favicon error

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -7,6 +7,7 @@ function App({ Component, pageProps }) {
   return <Fragment>
       <Head>
         <title>Ultimate Grid Data</title>
+        <link rel="shortcut icon" href="/favicon.png" />
       </Head>
       <Component {...pageProps} />
     </Fragment>
